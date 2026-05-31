3f09dceddce9f6e47f6ac11d861a5d5fd8a482bd
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-05-31 23:57:35 +0200

    feat(winui): introduce Application, AppBuilder, and UIThread core application lifecycle engine

---
+++ src/ProGPU.WinUI/Core/Application.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Microsoft.UI.Xaml;
+
+public class LaunchActivatedEventArgs : EventArgs
+{
+    public string[] Arguments { get; }
+
+    public LaunchActivatedEventArgs(string[] args)
+    {
+        Arguments = args;
+    }
+}
+
+public class Application
+{
+    public static Application Current { get; internal set; } = null!;
+
+    public event EventHandler<UnhandledExceptionEventArgs>? UnhandledException;
+
+    protected virtual void OnLaunched(LaunchActivatedEventArgs args)
+    {
+    }
+
+    internal void Launch(LaunchActivatedEventArgs args)
+    {
+        try
+        {
+            OnLaunched(args);
+        }
+        catch (Exception ex)
+        {
+            UnhandledException?.Invoke(this, new UnhandledExceptionEventArgs { Exception = ex });
+            Console.WriteLine($"[Application] Unhandled exception during launch: {ex.Message}");
+        }
+    }
+}
