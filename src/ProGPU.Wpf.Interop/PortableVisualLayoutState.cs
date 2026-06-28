7979c2be765d6eedfcb679e90761ccaa99d0b49e
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-06-28 22:42:33 +0200

    Add WPF visual layout state interop contract

---
+++ src/ProGPU.Wpf.Interop/PortableVisualLayoutState.cs
@@ -0,0 +1,17 @@
+namespace ProGPU.Wpf.Interop;
+
+public interface IPortableVisualLayoutStateSource
+{
+    bool TryGetPortableVisualLayoutState(out PortableVisualLayoutState state);
+}
+
+public sealed class PortableVisualLayoutState
+{
+    public bool HasRenderSize { get; set; }
+
+    public PortableSize RenderSize { get; set; }
+
+    public bool HasClipToBounds { get; set; }
+
+    public bool ClipToBounds { get; set; }
+}
